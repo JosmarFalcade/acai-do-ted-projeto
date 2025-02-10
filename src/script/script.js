6e209f712ab1ae640142e9bd5a115499bd3fed81
JosmarFalcade <josmarfalcade1996@gmail.com>
2025-02-10 13:50:21 -0300

    backup

--- src/script/script.js
+++ src/script/script.js
@@ -2,15 +2,12 @@ let count = 1;
 document.getElementById("radio1").checked = true;
 
 setInterval( function(){
-    nextImage();
+nextImage();
 },5000)
 
 function nextImage(){
-    count++;
-    if(count>4){
-        count = 1;
-    }
-    document.getElementById("radio"+count).checked = true;
-
-  
+count++;
+if(count>4){
+    count = 1;
 }
+document.getElementById("radio"+count).checked = true;}
